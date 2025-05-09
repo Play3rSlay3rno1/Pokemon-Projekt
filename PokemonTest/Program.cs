1958736e05d986469fab2a75a37a58a83cab573e
Richter <patrick.richter@training.lug-ag.de>
2025-05-09 11:28:37 +0200

    first commit

---
+++ PokemonTest/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace PokemonTest
+
+//Anforderungen und Ideen für das Spiel
+Geplanter Zeitraum : Montag
+//1. Einstieg & Charaktererstellung (Begrüßung und Abfrage ob Junge oder Mädchen)
+//Spielername (Spierlname eingabe)
+//Rivalenname (Rivalenname eingabe)
+//Auswahl des Starter-Pokémon (Auswahl zwischen 3 Pokémon)
+Geplanter Zeitraum : Dienstag
+//2. Pokémon-System
+//Pokémon-Klasse mit Typ, Level, KP, Attacken (Name, Attaken, Level, Kraftpunkte)
+//Typenstärken/-schwächen (Typenberechnung bei Kämpfen)
+//Fangsystem mit Fangchance (Pokémon fangen Chance berechnen nach KP)
+Geplanter Zeitraum : Mittwoch
+//3. Kampf-System
+//Trainerkämpfe (Zwei Trainerkämpfe auf dem Weg in die erste Stadt mit jeweils 1 Pokemon)
+//Rivalenkämpfe (Erster Kampf nach Starterauswahl, zweiter Kampf vor der Arena)
+//Arenakampf erste Arena (Zwei Trainer mit 2 Pokemon und Arenaleiter mit 3 Pokemon)
+Geplanter Zeitraum : Donnerstag
+//4. Welt & Navigation
+//Orte: Startstadt, Route, Arena-Stadt (Anfangsstadt, Route 1, Zweite Stadt Arena)
+//Poke-Center & Markt (Auswahlkriterien für die zweite Stadt)
+//Nebenmissionen (z.B. Items sammeln, Pokémon fangen)
+Geplanter Zeitraum : Freitag
+//5. Inventar & Wirtschaft (Tasche mit 3 Fächern)
+//Pokedollar als Währung (Startguthaben und Geld verdienen durch Kämpfe)
+//Items (Pokébälle, Tränke
+//Teamverwaltung (Zwei Pokemon fangen vor der Arena und ein Team aus 3 besitzen)
+Geplanter Zeitraum : Zwischendurch
+//Laufweg und Laufrichtung (Gras betreten, Trainer treffen, Items finden)
+Geplanter Zeitraum : Heute
+//Informationsbeschaffung
+
+
+
+    using System;
+using System.Collections.Generic;
+
+namespace TextPokemonAdventure
+{
+    
